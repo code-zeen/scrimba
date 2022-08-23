e6bb69617d895173d4a093bbf7ec53654ceba763
aezeen <aejin.km@gmail.com>
2022-08-23 14:28:25 +0900

    add files

---
+++ blackjack-game-app/script.js
@@ -0,0 +1,20 @@
+let firstCard = 11
+let secondCard = 10
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+if (sum < 21) {
+  message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+  message = "Wohoo! You won."
+  hasBlackJack = true
+} else {
+  message = "You lost all your money"
+  isAlive = false
+}
+
+
+
+console.log(message)
