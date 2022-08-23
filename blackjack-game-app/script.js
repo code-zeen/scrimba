9cf8f667192f41c33713a6a5c867cdaccf54be55
aezeen <aejin.km@gmail.com>
2022-08-24 02:06:31 +0900

    update interface to show cards and sum

--- blackjack-game-app/script.js
+++ blackjack-game-app/script.js
@@ -1,13 +1,15 @@
 let firstCard = 11
-let secondCard = 10
+let secondCard = 1
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
-
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+  console.log("clicked")
   if (sum < 21) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
@@ -17,6 +19,7 @@ function startGame() {
     message = "You lost all your money"
     isAlive = false
   }
+  messageEl.textContent = message
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+  sumEl.textContent = "Sum: " + sum
 }
-
-console.log(message)
