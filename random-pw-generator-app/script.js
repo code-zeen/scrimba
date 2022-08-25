b9c203c044c308e778fc6a2b51ffced0cd01b061
aezeen <aejin.km@gmail.com>
2022-08-25 09:40:00 +0900

    make "click to copy" - "copied!"  button reset on new pw generation

--- random-pw-generator-app/script.js
+++ random-pw-generator-app/script.js
@@ -1,34 +1,41 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+  "/"
+];
 
-let optionOne = document.getElementById("option-one")
-let optionTwo = document.getElementById("option-two")
+let options = document.getElementsByClassName("options")
+let optionOne = options[0]
+let optionTwo = options[1]
 
 function generatePw() {
-optionOne.textContent = ""
-optionTwo.textContent = ""
+  optionOne.textContent = ""
+  optionTwo.textContent = ""
 
- for (let i = 0; i < 15; i++) {
-  let randomIndex = Math.floor((Math.random()) * characters.length)
-   
-  optionOne.textContent += characters[randomIndex]
- }
- for (let i = 0; i < 15; i++) {
-  let randomIndex = Math.floor((Math.random()) * characters.length)
-   
-  optionTwo.textContent += characters[randomIndex]
- }
+  // tooltip reset
+  for (let i = 0; i < toolTip.length; i++) {
+    toolTip[i].textContent = "Click to copy"
+  }
+
+  // actual function
+  for (let i = 0; i < 15; i++) {
+    let randomIndex = Math.floor((Math.random()) * characters.length)
+
+    optionOne.textContent += characters[randomIndex]
+  }
+  for (let i = 0; i < 15; i++) {
+    let randomIndex = Math.floor((Math.random()) * characters.length)
+
+    optionTwo.textContent += characters[randomIndex]
+  }
 }
 
 // light/dark mode button
 let container = document.querySelector(".container")
-let lightDarkBtn =  document.getElementById("light-dark-btn")
+let lightDarkBtn = document.getElementById("light-dark-btn")
 let title = document.getElementById("title")
 let para = document.querySelector("p")
 let passwords = document.getElementById("passwords")
-
+// default darkmode
 let isDarkMode = true
-
 lightDarkBtn.textContent = "☀️"
 
 function switchMode() {
@@ -51,7 +58,7 @@ function switchMode() {
     title.classList.toggle("light-theme")
     para.classList.toggle("light-theme")
     passwords.classList.toggle("light-theme")
-    
+
     isDarkMode = true
   }
 }
@@ -59,17 +66,19 @@ function switchMode() {
 // copy to clipboard on click
 let toolTip = document.getElementsByClassName("tooltip")
 
+
 for (let i = 0; i < toolTip.length; i++) {
   toolTip[i].textContent = "Click to copy"
 }
 
 function copyToClipboardOne() {
-  let copyText = document.getElementById("option-one").textContent
+  let copyText = options[0].textContent
   navigator.clipboard.writeText(copyText)
   toolTip[0].textContent = "Copied!"
 }
+
 function copyToClipboardTwo() {
-  let copyText = document.getElementById("option-two").textContent
+  let copyText = options[1].textContent
   navigator.clipboard.writeText(copyText)
   toolTip[1].textContent = "Copied!"
 }
