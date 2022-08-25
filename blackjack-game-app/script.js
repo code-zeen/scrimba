e38c392b3b18dfa75a9d86b1844401e48c79b00f
aezeen <aejin.km@gmail.com>
2022-08-25 12:52:56 +0900

    add prompt for player name and fix minor bugs

--- blackjack-game-app/script.js
+++ blackjack-game-app/script.js
@@ -7,29 +7,51 @@ let isAlive = false
 let choseStay = false
 let message = ""
 
+// DOM elements
 let playerCardsEl = document.getElementById("player-cards-el")
 let playerSumEl = document.getElementById("player-sum-el")
 let dealerCardsEl = document.getElementById("dealer-cards-el")
 let dealerSumEl = document.getElementById("dealer-sum-el")
-
 let messageEl = document.getElementById("message-el")
 let playerEl = document.getElementById("player-el")
 
+// interface (text) automation
+function displayPlayerSum() {
+  playerSumEl.textContent = "Total: " + playerSum
+}
+
+function displayPlayerChips() {
+  playerEl.textContent = player.name + ": $" + player.chips
+}
+
+function displayDealerSum() {
+  dealerSumEl.textContent = "Total: " + dealerSum
+}
+
+function winChips() {
+  player.chips += 10
+}
+
+function loseChips() {
+  player.chips -= 10
+}
+
 // player profile
 let player = {
-  name: "Codezeen",
+  name: "John Doe",
   chips: 150
 }
 
 // interface
-playerEl.textContent = player.name + ": $" + player.chips
+player.name = prompt("Please enter your name: ", "John")
+displayPlayerChips()
 
 
 // generate a random card
 function getRandomCard() {
   let randomCard = Math.floor(Math.random() * 13) + 1
   if (randomCard === 1) {
-    if (playerSum < 11) {
+    if (playerSum < 11 || dealerSum < 11) {
       return 11
     } else {
       return 1
@@ -41,52 +63,71 @@ function getRandomCard() {
   }
 }
 
-// start OR continue
+// start / continue
 function startGame() {
-  isAlive = true
-  hasBlackJack = false
-  choseStay = false
+  // cannot start/restart game if alive
+  if (isAlive === false) {
+    // reset status
+    isAlive = true
+    hasBlackJack = false
+    choseStay = false
+    playerCardsEl.textContent = "Player: "
 
-  let playerFirstCard = getRandomCard()
-  let playerSecondCard = getRandomCard()
-  playerCards = [playerFirstCard, playerSecondCard]
-  playerSum = playerFirstCard + playerSecondCard
+    // must have money to play
+    if (player.chips > 0) {
+      // initialize player
+      let playerFirstCard = getRandomCard()
+      let playerSecondCard = getRandomCard()
+      playerCards = [playerFirstCard, playerSecondCard]
+      playerSum = playerFirstCard + playerSecondCard
+      // initialize dealer
+      let dealerFirstCard = getRandomCard()
+      dealerCards = [dealerFirstCard]
+      dealerSum = dealerFirstCard
+      dealerCardsEl.textContent = "Dealer: " + dealerFirstCard + " "
 
-  let dealerFirstCard = getRandomCard()
-  dealerCards = [dealerFirstCard]
-  dealerSum = dealerFirstCard
-
-  dealerCardsEl.textContent = "Dealer: " + dealerFirstCard + " "
-  renderGame()
-}
-
-// calculate the game
-function renderGame() {
-  if (player.chips > 0) {
-    playerCardsEl.textContent = "Cards: "
-
-    for (let i = 0; i < playerCards.length; i++) {
-      playerCardsEl.textContent += playerCards[i] + " "
-    }
-
-    console.log("rendered")
-    if (playerSum <= 21) {
-      message = "Do you want to draw a new card?"
+      // initial blackjack
+      if (playerSum === 21) {
+        playerCardsEl.textContent += playerFirstCard + " " + playerSecondCard
+        messageEl.textContent = "BLACKJACK!!"
+        displayPlayerSum()
+        winChips()
+        displayPlayerChips()
+        hasblackJack = true
+        // continue actual game
+      } else {
+        renderGame()
+      }
+      // no money
     } else {
-      message = "Bust! You lost $10"
-      player.chips -= 10
-      isAlive = false
+      messageEl.textContent = "Sorry, you're out of money"
     }
-
-    playerEl.textContent = player.name + ": $" + player.chips
-    messageEl.textContent = message
-    playerSumEl.textContent = "Total: " + playerSum
-
-    dealerSumEl.textContent = "Total: " + dealerSum
   } else {
-    messageEl.textContent = "Sorry, you're out of money"
+    console.log("Please finish the current game")
   }
 }
+
+// actual game
+function renderGame() {
+  for (let i = 0; i < playerCards.length; i++) {
+    playerCardsEl.textContent += playerCards[i] + " "
+  }
+
+  console.log("rendered")
+  if (playerSum <= 21) {
+    message = "Do you want to draw a new card?"
+  } else {
+    message = "Bust! You lost $10"
+    loseChips()
+    isAlive = false
+  }
+
+  messageEl.textContent = message
+  displayPlayerChips()
+  displayPlayerSum()
+  displayDealerSum()
+}
+
 
 // draw a card (hit)
 function newCard() {
@@ -103,38 +144,42 @@ function newCard() {
 }
 
 function stay() {
-  choseStay = true
-  for (let i = 0; i < 10; i++) {
-    if (dealerSum < 18) {
-      let newCard = getRandomCard()
-      dealerCards.push(newCard)
-      dealerSum += newCard
-      dealerCardsEl.textContent += dealerCards[i] + " "
-      dealerSumEl.textContent = "Total: " + dealerSum
+  if (isAlive === true && hasBlackJack === false) {
+    choseStay = true
+    for (let i = 0; i < 10; i++) {
+      if (dealerSum < 18) {
+        let newCard = getRandomCard()
+        dealerCards.push(newCard)
+        dealerSum += newCard
+        dealerCardsEl.textContent += dealerCards[i] + " "
+        dealerSumEl.textContent = "Total: " + dealerSum
+      }
     }
-  }
-  if (dealerSum === 21) {
-    if (playerSum === 21) {
-      messageEl.textContent = "It's a tie!"
-    } else {
-      messageEl.textContent = "You lost $10"
-      player.chips -= 10
-      isAlive = false
-    }
-  } else if (dealerSum > 21) {
-    messageEl.textContent = "The dealer busted! You won $10"
-    player.chips += 10
-    hasBlackJack = true
-  } else if (dealerSum < 21) {
-    if (playerSum > dealerSum) {
-      messageEl.textContent = "You won $10!!"
+    if (dealerSum === 21) {
+      if (playerSum === 21) {
+        messageEl.textContent = "It's a tie!"
+      } else {
+        messageEl.textContent = "You lost $10"
+        player.chips -= 10
+        isAlive = false
+      }
+    } else if (dealerSum > 21) {
+      messageEl.textContent = "The dealer busted! You won $10"
       player.chips += 10
-    } else if (playerSum < dealerSum) {
-      messageEl.textContent = "You lost $10"
-      player.chips -= 10
-    } else if (playerSum === dealerSum) {
-      messageEl.textContent = "It's a tie!"
+      hasBlackJack = true
+    } else if (dealerSum < 21) {
+      if (playerSum > dealerSum) {
+        messageEl.textContent = "You won $10!!"
+        player.chips += 10
+      } else if (playerSum < dealerSum) {
+        messageEl.textContent = "You lost $10"
+        player.chips -= 10
+      } else if (playerSum === dealerSum) {
+        messageEl.textContent = "It's a tie!"
+      }
     }
+    playerEl.textContent = player.name + ": $" + player.chips
+  } else {
+    console.log("Please restart the game")
   }
-  playerEl.textContent = player.name + ": $" + player.chips
 }
