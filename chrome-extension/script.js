d0fb1de16decd05a3e03faf960d8b94a035888de
aezeen <aejin.km@gmail.com>
2022-08-30 11:51:13 +0900

    refactor render function with parameters

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,49 +1,45 @@
+let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
-let myLeads = []
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)  // check
 
 if (leadsFromLocalStorage) {
-  console.log("truthy") // check
   myLeads = leadsFromLocalStorage
-  renderLeads()
-} else {
-  console.log("falsey") // check
+  render(myLeads)
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-  console.log("User has double-clicked 'DELETE ALL'")
-  localStorage.clear()
-  myLeads = []
-  renderLeads()
-})
-inputBtn.addEventListener("click", function () {
-  console.log("User has clicked 'SAVE INPUT'")
-
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-
-  console.log(myLeads)
-  renderLeads()
-
-  console.log(localStorage.getItem("myLeads"))
-})
-
-function renderLeads() {
+function render(leads) {
   let listItems = ""
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a href="${myLeads[i]}" target="_blank">
-          ${myLeads[i]}
+        <a href="${leads[i]}" target="_blank">
+          ${leads[i]}
         </a>
       </li>
       `
   }
   ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+  console.log("User has dbl-clicked 'DELETE ALL'")
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+  console.log("User has clicked 'SAVE INPUT'")
+
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+
+  console.log(localStorage.getItem("myLeads"))
+})
