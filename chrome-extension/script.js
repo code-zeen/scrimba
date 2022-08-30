27ff75cc1c6f79f84484909e01d08a823a74c997
aezeen <aejin.km@gmail.com>
2022-08-30 10:44:55 +0900

    add functioning delete button

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,27 +1,49 @@
-const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 let myLeads = []
 
-inputBtn.addEventListener("click", function() {
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)  // check
+
+if (leadsFromLocalStorage) {
+  console.log("truthy") // check
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+} else {
+  console.log("falsey") // check
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+  console.log("User has double-clicked 'DELETE ALL'")
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
+inputBtn.addEventListener("click", function () {
   console.log("User has clicked 'SAVE INPUT'")
+
   myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
   console.log(myLeads)
   renderLeads()
-  inputEl.value = ""
+
+  console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-  listItems +=`"
-  <li>
-    <a href='${myLeads[i]}' target='_blank'>
-      ${myLeads[i]}
-    </a>
-  </li>
-  `
-}
-ulEl.innerHTML = listItems
-console.log(listItems)
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+      <li>
+        <a href="${myLeads[i]}" target="_blank">
+          ${myLeads[i]}
+        </a>
+      </li>
+      `
+  }
+  ulEl.innerHTML = listItems
 }
