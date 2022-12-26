eb9707557930253da82c50f33620bf831db9a761
aezeen <aejin.km@gmail.com>
2022-12-26 16:37:24 +0900

    test logo image source 3

--- react/meme-generator/src/components/Header.jsx
+++ react/meme-generator/src/components/Header.jsx
@@ -5,10 +5,10 @@ export default function Header() {
     <header className='header'>
       <div className="container">
         <img 
-          src='../../src/assets/troll-face.png' 
+          src='../../src/assets/trollface.png' 
           className='image'
         />
-        <h2 className='title'>Meme test 2 Generator</h2>
+        <h2 className='title'>Meme test 3 Generator</h2>
         <h4 className='project'>Project 3</h4>
       </div>
     </header>
