f35e4e0860ad00c1b946ec6fb325515aa9fc9339
aezeen <aejin.km@gmail.com>
2022-12-26 15:23:07 +0900

    add max-width and centralize content

--- react/meme-generator/src/components/Header.jsx
+++ react/meme-generator/src/components/Header.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='header'>
-      <img 
-        src='./src/assets/troll-face.png' 
-        className='image'
-      />
-      <h2 className='title'>Meme Generator</h2>
-      <h4 className='project'>Project 3</h4>
+      <div className="container">
+        <img 
+          src='./src/assets/troll-face.png' 
+          className='image'
+        />
+        <h2 className='title'>Meme Generator</h2>
+        <h4 className='project'>Project 3</h4>
+      </div>
     </header>
   )
 }
