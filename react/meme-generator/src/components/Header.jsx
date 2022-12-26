680e1d2dc5d5b59c6b1c0dce383b9595ffbb8ece
aezeen <aejin.km@gmail.com>
2022-12-26 16:57:05 +0900

    test logo image source 6

--- react/meme-generator/src/components/Header.jsx
+++ react/meme-generator/src/components/Header.jsx
@@ -5,10 +5,10 @@ export default function Header() {
     <header className='header'>
       <div className="container">
         <img 
-          src='https://63a950104f3f6b00082d2ae3--039e5db6-6ee4-4e28-a34e-007e6b384d6b.netlify.app/assets/troll-face-11d1702d.png' 
+          src='../assets/troll-face-11d1702d.png' 
           className='image'
         />
-        <h2 className='title'>Meme test 5 Generator</h2>
+        <h2 className='title'>Meme test 6 Generator</h2>
         <h4 className='project'>Project 3</h4>
       </div>
     </header>
