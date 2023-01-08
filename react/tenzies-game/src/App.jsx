732ef4ebf6eae79d99dea49c627bbe811c0076fe
aezeen <aejin.km@gmail.com>
2023-01-09 06:04:38 +0900

    initiate dice in state

--- react/tenzies-game/src/App.jsx
+++ react/tenzies-game/src/App.jsx
@@ -2,23 +2,43 @@ import React from 'react'
 import Die from './components/Die'
 
 export default function App() {
+
+  const [dice, setDice] = React.useState(allNewDice())
+
+  const diceElements = dice.map(
+    die => <Die key={die.id} value={die.value}/>
+    )
+
+  function getRandomDie() {
+    return Math.floor(Math.random() * 6) + 1
+  }
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        id: i + 1,
+        value: getRandomDie(),
+        isHeld: false
+      })
+    }
+    return newDice
+  }
+
+  console.log(dice)
+
+  function rerollDice() {
+    setDice(allNewDice())
+  }
+
   return (
     <main>
       <h1 className="title">Tenzies</h1>
       <p className="how-to-play">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice">
-        <Die/>
-        <Die/>
-        <Die/>
-        <Die/>
-        <Die/>
-        <Die/>
-        <Die/>
-        <Die/>
-        <Die/>
-        <Die/>
+        {diceElements}
       </div>
-      <button>Roll</button>
+      <button onClick={rerollDice}>Roll</button>
     </main>
   )
 }
--- react/tenzies-game/src/components/Die.jsx
+++ react/tenzies-game/src/components/Die.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
 export default function Die(props) {
-  function getRandomDie() {
-    return Math.floor(Math.random() * 6) + 1
-  }
-
-  const randomDie = getRandomDie()
+  
 
   return (
     <div className="die">
-      {randomDie}
+      {props.value}
     </div>
   )
 }
