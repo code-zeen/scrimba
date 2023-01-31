cb34147c33b80a0bfe13c04a10823457895a73a0
aezeen <aejin.km@gmail.com>
2023-01-31 17:43:01 +0900

    add background blobs

---
+++ react/open-trivia-trial-2/src/components/loading.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Loading() {
+  return (
+    <div className="loading-screen">
+      <div className="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  )
+}
