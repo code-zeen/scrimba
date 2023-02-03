46a7c61f63a687d9d2f6d853cdf5b587d04eafc8
aezeen <aejin.km@gmail.com>
2023-02-04 03:14:40 +0900

    set basic functionality

--- scrimba-live/stopwatch/index.js
+++ scrimba-live/stopwatch/index.js
@@ -1,6 +1,6 @@
 const startBtn = document.getElementById("start")
 const stopBtn = document.getElementById("stop")
-const pauseBtn = document.getElementById("pause")
+const resetBtn = document.getElementById("reset")
 
 let time = document.getElementById("time")
 
@@ -9,17 +9,23 @@ let ms = 0
 let s = 0
 let m = 0
 
-const intervalId = setInterval(start, 100)
-// clearInterval(intervalId)
+let intervalId
 
 function start() {
-  total++
-  ms = total
-  s = Math.floor(total / 10)
-  m = Math.floor(total/ 600)
-  time.innerText = `${m}:${s}:${ms}`
+  intervalId = setInterval(function() {
+    total++
+    ms = total % 100
+    s = Math.floor(total / 100) % 60
+    m = Math.floor(total / 6000)
+    time.innerText = `${m}:${s}:${ms}`
+  }, 10)
 }
 
 
-
+startBtn.addEventListener("click", () => start())
 stopBtn.addEventListener("click", () => clearInterval(intervalId))
+resetBtn.addEventListener("click", () => {
+  total = 0
+  clearInterval(intervalId)
+  time.innerText = "00:00:00"
+})
