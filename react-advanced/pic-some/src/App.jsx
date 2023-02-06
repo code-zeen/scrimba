fcc457adfcc33c75118caac295134cef7167bd98
aezeen <aejin.km@gmail.com>
2023-02-07 06:26:42 +0900

    upload initial files with basic Links

---
+++ react-advanced/pic-some/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { Link, Switch, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Photos from "./pages/Photos"
+import Cart from './pages/Cart'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Home Page</h1>
+
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
+      
+    </div>
+  )
+}
+
+export default App
