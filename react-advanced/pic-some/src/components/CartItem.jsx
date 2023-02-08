3c7fddd8632f1cced2589098f2509ce2fad6ab7e
aezeen <aejin.km@gmail.com>
2023-02-09 04:57:47 +0900

    make minor UI improvements

--- react-advanced/pic-some/src/components/CartItem.jsx
+++ react-advanced/pic-some/src/components/CartItem.jsx
@@ -1,13 +1,20 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 
 import { Context } from "../Context"
 
 function CartItem(props) {
+  const [isHovered, setIsHovered] = useState(false)
   const {removeFromCart} = useContext(Context)
 
+  const iconClassName = isHovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
   return (
     <div className="cart-item">
-      <i className="ri-delete-bin-line" onClick={() => removeFromCart(props.item)}></i>
+      <i 
+        className={iconClassName} 
+        onClick={() => removeFromCart(props.item)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      ></i>
       <img src={props.item.url} width="180px" />
       <p>$5.99</p>
     </div>
