355dedcf7f0889ba7a7fe9a1fcb3b6eeb43a42e2
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-08-12 16:41:12 +0900

    update image url

--- react/travel-journal/src/data.js
+++ react/travel-journal/src/data.js
@@ -6,7 +6,7 @@ export default [
       startDate: "2021년 1월 12일",
       endDate: "1월 24일",
       description: "사랑과 이런 된 내 별들을 내 언덕 시와 그리고 있습니다. 쉬이 별 이웃 다하지 많은 하나에 듯합니다. 별들을 우는 멀리 거외다.",
-      imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+      imageUrl: "https://res.cloudinary.com/jnto/image/upload/w_750,h_503,fl_lossy,f_auto/v1531981666/fujiguide/SG010_6"
   },
   {
       title: "시드니 오페라 하우스",
@@ -15,7 +15,7 @@ export default [
       startDate: "2021년 5월 27일",
       endDate: "6월 08일",
       description: "어머니 잠, 당신은 언덕 봅니다. 하나의 딴은 어머님, 어머님, 토끼, 별 언덕 한 까닭입니다. 이네들은 잠, 나는 다 우는 된 내일 노새, 계십니다.",
-      imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+      imageUrl: "https://miro.medium.com/v2/resize:fit:4250/1*-MklWDSjKS5vWEG5ZYXCww.jpeg"
   },
   {
       title: "예이랑에르",
@@ -24,6 +24,6 @@ export default [
       startDate: "2021년 10월 1일",
       endDate: "11월 18일",
       description: "다 토끼, 헤는 벌레는 때 했던 계십니다. 봄이 불러 경, 있습니다. 헤는 쓸쓸함과 나의 다 사랑과 묻힌 위에 노루, 있습니다.",
-      imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+      imageUrl: "https://www.hotelscombined.co.kr/rimg/dimg/36/fd/c1871f2d-freereg-1758-16a797b2e99.jpg?width=1366&height=768&xhint=1998&yhint=1667&crop=true"
   },
 ]
